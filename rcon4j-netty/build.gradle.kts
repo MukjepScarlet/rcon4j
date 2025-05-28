df7778b54423db0254eb0298fc4d6b83f2418142
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-05-28 16:57:05 +0800

    netty module

---
+++ rcon4j-netty/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(project(":rcon4j-core"))
+    api("io.netty:netty-transport:4.1.197.Final")
+    api("io.netty:netty-buffer:4.1.197.Final")
+    api("io.netty:netty-common:4.1.197.Final")
+    api("io.netty:netty-resolver:4.1.197.Final")
+    api("io.netty:netty-handler:4.1.197.Final")
+    compileOnly("org.jetbrains:annotations:24.0.0")
+}
+
+group = rootProject.group
+version = rootProject.version
+
+tasks.jar {
+    archiveBaseName = project.name
+    manifest {
+        attributes["Implementation-Title"] = project.name
+        attributes["Implementation-Version"] = version
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ rcon4j-netty/src/main/java/moe/lasoleil/rcon4j/ByteBufRconPacketAdapter.java
@@ -0,0 +1,41 @@
+package moe.lasoleil.rcon4j;
+
+import io.netty.buffer.ByteBuf;
+import moe.lasoleil.rcon4j.exceptions.MalformedPacketException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public final class ByteBufRconPacketAdapter implements RconPacket.Writer<ByteBuf>, RconPacket.Reader<ByteBuf> {
+
+    public static final ByteBufRconPacketAdapter INSTANCE = new ByteBufRconPacketAdapter();
+
+    private ByteBufRconPacketAdapter() {}
+
+    @NotNull
+    @Override
+    public RconPacket read(@NotNull ByteBuf in) throws IOException {
+        int length = in.readIntLE();
+        int id = in.readIntLE();
+        int type = in.readIntLE();
+        byte[] payload = new byte[length - 4 - 4 - 2];
+        if (in.readableBytes() < payload.length) {
+            throw new MalformedPacketException("Excepted payload length=" + payload.length + ", real length=" + in.readableBytes());
+        }
+        in.readBytes(payload);
+        if (in.readByte() != 0 || in.readByte() != 0) {
+            throw new MalformedPacketException("Should be packet end");
+        }
+        return Util.createS2CPacket(id, type, payload);
+    }
+
+    @Override
+    public void write(@NotNull ByteBuf out, @NotNull RconPacket packet) throws IOException {
+        out.writeIntLE(packet.length())
+                .writeIntLE(packet.id())
+                .writeIntLE(packet.type())
+                .writeBytes(packet.payload())
+                .writeByte(0)
+                .writeByte(0);
+    }
+}
